f9d644c0a2941ea1082eab01c51503b0c7abe928
guy32807 <guy32807@comcast.net>
2021-01-14 15:10:02 -0500

    Calculate the interest rate based on principal, year and APR

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
 function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100  
+    var result = document.getElementById("result").value;
+    var interest = principal * parseInt(years) * rate/100
+    var currentYear = new Date()
+    var futureyear = currentYear.getFullYear() + parseInt(years);
+    console.log(futureyear)
+
+    if(!isNaN(principal) && principal > 0){
+        document.getElementById("result").innerHTML = "If you deposit " + formatter.format(principal) + ", at an interest rate of " + rate + "%. You will receive an amount of " + formatter.format(interest) + ", in the year " + futureyear;
+    }
+    else{
+        alert("Please enter a valid numeric dollar amount!")
+    }
+ 
 }
-        
+
+function updateRate(val){
+    document.getElementById("inputtext").value = val;
+}
